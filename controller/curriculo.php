3d1b5311de76fed70c76c9f9341092dffed6d784
Evelin1005 <evelin.sacramento@escola.pr.gov.br>
2025-07-29 09:03:21 -0300

    Update curriculo.php

--- controller/curriculo.php
+++ controller/curriculo.php
@@ -1,10 +1,21 @@
 <?php
-$nome = $_["nome"];
-$nome = $_["telefone"];
-$nome = $_["nome"];
-$nome = $_["nome"];
-
-
-
+$nome = $_POST["nome"];
+$telefone = $_POST["telefone"];
+$endereco = $_POST["endereco"];
+$email = $_POST["email"];
+$efc = $_POST["efc"];
+$emc = $_POST["emc"];
+$esc = $_POST["esc"];
+$pgc = $_POST["pgc"];
+$mc = $_POST["mc"];
+$empresa = $_POST["empresa"];
+$data_admissao = $_POST["data_admissao"];
+$data_demissao = $_POST["data_demissao"];
+$atividade_exercida = $_POST["atividade_exercida"];
 
 ?>
+
+
+
+
+
